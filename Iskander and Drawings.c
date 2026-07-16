3028223d9275cf321396c47c7808938a91c50705
Sree <vrsram77@gmail.com>
2026-07-16 21:59:49 +0530

    Auto-sync 2026-07-16 21:59:49

--- Iskander and Drawings.c
+++ Iskander and Drawings.c
@@ -1,1 +1,34 @@
-hi 
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    while (t--) {
+        int n;
+        char s[20];
+
+        scanf("%d", &n);
+        scanf("%s", s);
+
+        int currentLength = 0;
+        int maximumLength = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '#') {
+                currentLength++;
+
+                if (currentLength > maximumLength) {
+                    maximumLength = currentLength;
+                }
+            } else {
+                currentLength = 0;
+            }
+        }
+
+        int answer = (maximumLength + 1) / 2;
+        printf("%d\n", answer);
+    }
+
+    return 0;
+}
