db42f45ec61f4a85e7cc6602f9b8dd2cf17baacf
Sree <vrsram77@gmail.com>
2026-07-17 14:49:19 +0530

    Auto-sync 2026-07-17 14:49:19

--- StaticRange Sum Queries.c
+++ StaticRange Sum Queries.c
@@ -4,21 +4,15 @@ int main(void) {
     scanf("%d %d", &n, &q);
     long long prefix[n + 1];
     prefix[0] = 0;
-
     for (int i = 1; i <= n; i++) {
         long long value;
         scanf("%lld", &value);
-
         prefix[i] = prefix[i - 1] + value;
     }
-
     while (q--) {
         int a, b;
-
         scanf("%d %d", &a, &b);
-
         long long sum = prefix[b] - prefix[a - 1];
-
         printf("%lld\n", sum);
     }
 
